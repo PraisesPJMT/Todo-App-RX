fa067b2cfe83ed06872c647e1755967d0a1a75d5
PraisesPJMT <praisesmusa@outlook.com>
2022-09-12 13:40:02 +0100

    Part-1: Beginners Guide

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import TodoContainer from './components/TodoContainer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+    <TodoContainer />
   </React.StrictMode>,
 );
