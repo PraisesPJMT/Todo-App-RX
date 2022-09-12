ea78ff1d66463260ceeec0e4bada67b3ae787326
PraisesPJMT <praisesmusa@outlook.com>
2022-09-12 17:42:30 +0100

    Part-7: Understanding React Hooks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './App.css';
-import TodoContainer from './components/TodoContainer';
+import TodoContainer from './functionBased/components/TodoContainer';
+import './functionBased/App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
