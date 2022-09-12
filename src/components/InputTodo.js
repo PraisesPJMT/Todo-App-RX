741946c428b6223351c02f5e10f314356b8230bb
PraisesPJMT <praisesmusa@outlook.com>
2022-09-12 15:30:30 +0100

    Part-3: Implementing CSS in React

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -25,13 +25,16 @@ class InputTodo extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit}
+            className="form-container"
+      >
         <input type="text"
+               className="input-text"
                name="title"
                placeholder={this.state.title}
                onChange={this.handleChange}
         />
-        <button>Submit</button>
+        <button className="input-submit">Submit</button>
       </form>
     )
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import styles from './TodoItem.module.css';
 
 function TodoItem(props) {
+  const { completed, id, title } = props.todo;
+
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#595959",
+    opacity: 0.4,
+    textDecoration: "line-through",
+  }
+
   return (
-    <li>
+    <li className={styles.item}>
       <input type="checkbox"
-             checked={props.todo.completed}
+             className={styles.checkbox}
+             checked={completed}
              onChange={() => props.handleChangeProps(props.todo.id)}
-      /> {props.todo.title}
+      />
+      <span style={completed ? completedStyle : null}>
+        {title}
+      </span>
       <button onClick={() => props.deleteTodoProps(props.todo.id)}>
         Delete
       </button>
